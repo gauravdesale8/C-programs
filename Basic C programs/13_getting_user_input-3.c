004cc1f7c18d1a83c6f561f49b025b6ce531165d
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2020-01-04 23:40:55 +0530

    Add files via upload

---
+++ Basic C programs/13_getting_user_input-3.c
@@ -0,0 +1,11 @@
+/* getting user input - 3 */
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char grade;
+	printf("Enter your grade: ");
+	scanf("%c", &grade);
+	printf("Your grade is %c.",grade);
+	return 0;
+}
