0f7170f71021a52e3b8c5358f961da3f12db4e7b
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2020-01-05 13:16:46 +0530

    Add files via upload

---
+++ Basic C programs/20_arrays-2.c
@@ -0,0 +1,16 @@
+/* Arrays - 2 */
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int luckyNumbers[10];
+	luckyNumbers[0] = 80;
+	luckyNumbers[1] = 16;
+	luckyNumbers[2] = 25;
+	luckyNumbers[3] = 34;
+	luckyNumbers[4] = 44;
+	luckyNumbers[5] = 06;
+	printf("%d" , luckyNumbers[3]);
+	return 0;
+}
+
