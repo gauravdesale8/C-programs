7358e4e3747532d74fa60840c875779b2dd7c10b
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2020-01-05 16:06:18 +0530

    Add files via upload

---
+++ Basic C programs/21_return_statement-1.c
@@ -0,0 +1,13 @@
+/* Return Statement - 1  */
+#include<stdio.h>
+#include<stdlib.h>
+double cube(double num)
+{
+	double result = num * num * num;
+}
+
+int main()
+{
+	printf("Answer: %f",cube(3.0));
+	return 0;
+}
