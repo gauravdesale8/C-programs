b8d5bf3f0a193b94af802663ef9dc4b3f4f11ad4
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2020-01-06 00:53:04 +0530

    Add files via upload

---
+++ Basic C programs/27_if_statement-3.c
@@ -0,0 +1,15 @@
+/* if statement - 3 */
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	if(3 < 2 || 2 > 5) /* || is refered as "or" */ 
+	{
+		printf("True");
+	}
+	else
+	{
+		printf("False");
+	return 0;
+	}
+}
