ee65e5b6bb243be789ae7f6cc2fbb04a67900ace
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2020-01-05 13:09:09 +0530

    Add files via upload

---
+++ Basic C programs/19_arrays-1.c
@@ -0,0 +1,10 @@
+/* Arrays - 1 */
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int luckyNumbers[] = {4,8,15,16,23,42};
+	printf("%d", luckyNumbers[0]);
+       	/* luckyNumbers[0]=4,[1]=8,[2]=15,...[5]=42 */
+	return 0;
+}
