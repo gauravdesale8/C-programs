524edf499909ed836523278c1cab8fb976968380
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2020-01-05 16:25:02 +0530

    Add files via upload

---
+++ Basic C programs/24_functions.c
@@ -0,0 +1,13 @@
+/* Fubctions - 1 */
+#include<stdio.h>
+#include<stdlib.h>
+void sayHi(char name[]);
+int main()
+{
+	sayHi("Mike");
+	return 0;
+}
+
+void sayHi(char name[]){
+	printf("Hello %s",name);
+}
