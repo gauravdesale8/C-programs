a85336f22f9b5d2ea2f8143011d05aa3bfde6487
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2020-01-06 19:45:29 +0530

    Add files via upload

---
+++ Basic C programs/32_while_loop-1.c
@@ -0,0 +1,13 @@
+/* While Loop - 1 */
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+	int index = 1;
+	while(index <= 5)
+	{
+		printf("%d\n", index);
+		index++; /* index++ is imp */
+	}
+	return 0;
+}
