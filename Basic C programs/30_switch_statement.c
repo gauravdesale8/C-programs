b5231cb45f40d3c388038a539e046bcbb3e918eb
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2020-01-06 12:20:23 +0530

    Add files via upload

---
+++ Basic C programs/30_switch_statement.c
@@ -0,0 +1,29 @@
+/* Switch Statement */
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char grade = 'A'; /* A will excute = case 'A' */
+
+	switch(grade)
+	{
+		case 'A' :
+			printf("You did great! ");
+			break;
+		case 'B' :
+			printf("You did alright! ");
+			break;
+		case 'C' :
+			printf("You did poorly! ");
+			break;
+		case 'D' :
+			printf("You did very bad! ");
+			break;
+		case 'F' :
+			printf("You Failed! ");
+			break;
+		default :
+			printf("Invalid Grade");
+	}
+	return 0;
+}
